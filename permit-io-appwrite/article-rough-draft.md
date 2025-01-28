56a41c59816e2ab50cce9aab59a52dcc247d5f88
DeraCodings <otuonyechidera@yahoo.com>
2025-01-28 11:46:30 +0100

    Added permission check to the role assignment API endpoint: client-side

--- permit-io-appwrite/article-rough-draft.md
+++ permit-io-appwrite/article-rough-draft.md
@@ -1,9 +1,13 @@
 ## Article Rough draft
 
 ## Step one
+
 Create users and log in sessions in Appwrite
 Create an account on Permit.io
 Create roles and add actions (view, update, download, share)
 Download Docker
 Pull Permit.io docker pdp
 Run the container
+
+For the role assignment:
+Only users that uploaded the file can call the role assignment API endpoint.
--- permit-io-appwrite/src/actions/actions.ts
+++ permit-io-appwrite/src/actions/actions.ts
@@ -1,4 +1,4 @@
-import { database, Document } from "../configurations/appwrite";
+import { account, database, Document } from "../configurations/appwrite";
 import { updateSharedUserRole } from "../configurations/permit-io";
 
 export async function shareFile(
@@ -6,12 +6,23 @@ export async function shareFile(
   userEmail: string,
   role: string
 ) {
+  const user = await account.get();
+  console.log(user);
+  
   try {
     const currentDoc: Document = await database.getDocument(
       import.meta.env.VITE_DATABASE_ID,
       import.meta.env.VITE_FILE_METADATA_COLLECTION_ID,
       fileId
     );
+    
+    if(!user) throw new Error("Unauthorized: User not found");
+    
+    if(currentDoc.ownerId !== user.$id) throw new Error("Unauthorized: Only the owner can share the file");
+
+    if(currentDoc.shared_with.includes(userEmail)) throw new Error("User already has access to this file");
+
+    if(currentDoc.ownerId === user.$id) throw new Error("Cannot share file with owner");
 
     const result = await database.updateDocument(
       import.meta.env.VITE_DATABASE_ID,
